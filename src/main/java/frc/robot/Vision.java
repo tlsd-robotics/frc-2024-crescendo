689e19d53a5b36d7e301e5351e9cc7c394b68feb
Austin Mathes <35355378+JimB00nie@users.noreply.github.com>
2024-02-12 21:01:24 -0800

    config IDs

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -6,11 +6,11 @@ import TLsdLibrary.Vision.Target;
 public class Vision {
     //================== Limelights ========================
 
-        public static Limelight fronLimelight = new Limelight(0, "limelight", 22.0, 28.0);
+        // public static Limelight fronLimelight = new Limelight(0, "limelight", 22.0, 28.0);
     
     
 
     //==================== Targets ==========================
-        public static Target two = new Target(64.5, 0.25, 2);
+        // public static Target two = new Target(64.5, 0.25, 2);
     
 }
