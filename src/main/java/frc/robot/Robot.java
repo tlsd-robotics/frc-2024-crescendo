24bd82700054441eede908ffd9ff818faa24c8a7
Nolan <tlrobotics@toutlesd.org>
2024-03-13 16:24:28 -0700

    Fixed Pneumatics

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -55,6 +55,7 @@ public class Robot extends TimedRobot
 
     //superstructure.start();
     pneumatics.enableCompressorDigital();
+    pneumatics.clearStickyFaults();
     pdp.clearStickyFaults();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -7,22 +7,30 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ClimberSubsystem extends SubsystemBase {
   /** Creates a new Climber. */
 
-  DoubleSolenoid climberSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.Climber.FORWARD_CHANNEL, Constants.Climber.REVERSE_CHANNEL);
+  //DoubleSolenoid climberSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.Climber.FORWARD_CHANNEL, Constants.Climber.REVERSE_CHANNEL);
+  DoubleSolenoid climberSolenoid = new DoubleSolenoid(2, PneumaticsModuleType.REVPH, Constants.Climber.FORWARD_CHANNEL, Constants.Climber.REVERSE_CHANNEL);
 
   public void setExetended(boolean extended) {
-    if (extended)
+    if (extended) {
       climberSolenoid.set(Value.kForward);
-    else 
+      //SmartDashboard.putString("Climber State: ", "Extended");
+    }
+    else { 
       climberSolenoid.set(Value.kReverse);
+      //SmartDashboard.putString("Climber State: ", "Retracted");
+    }
   }
 
-  public ClimberSubsystem() {}
+  public ClimberSubsystem() {
+    //SmartDashboard.putString("Climber State: ", "Unknown");
+  }
 
   @Override
   public void periodic() {}
